1a6a1deaf2e9a1425d32c8d8b28cff66b3c34c7c
Arif Waquar <waquararif149@gmail.com>
2026-07-31 19:46:57 +0530

    fixes the cors cofig

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ import cors from "cors";
 const app=express();
 
 app.use(cors({
-  origin:[
-    "http://localhost:5173"
-  ]
-}))
+    origin: [
+        "http://localhost:5173"
+    ],
+    credentials: true
+}));
 
 app.use(express.json());
 app.use("/image",express.static("images"))
